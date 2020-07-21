aa06f3abaa0e1fad041a10a95c35f6c5b4f759f3
EANimesha <nimeshadilini999@gmail.com>
2020-07-21 22:27:21 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';
 import Home from './components/Home';
+import Login from './components/Login';
+
+import {Provider} from 'react-redux';
+import store from './redux/store';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <Home/>
+    <Router>
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/home" component={Home} />
+              </Switch>
+            </div>
+          </Router>
     </div>
+    </Provider>
   );
 }
 
